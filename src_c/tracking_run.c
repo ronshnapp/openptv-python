090a1ad4ef46a1bb1fa6ad61280dacfe5b7d4feb
Alex Liberzon <alex.liberzon@gmail.com>
2016-06-10 17:13:53 +0300

    this fix is a temporary workaround the problem of sequence.par and backward compatibility - Tcl/Tk doesn't know about other than num_cams = 4 case - openptv-python knows about arbitrary num_cams <= 4 and so for 1 camera case - sequence.par is not compatible with the new, smart reading which depends on cpar->num_cams - not sure if the hack should not be used also for 2 or 3 cameras.

--- src_c/tracking_run.c
+++ src_c/tracking_run.c
@@ -20,7 +20,12 @@ void tr_init(tracking_run *tr, char *seq_par_fname, char *tpar_fname,
     tr->tpar = read_track_par(tpar_fname);
     tr->vpar = read_volume_par(vpar_fname);
     tr->cpar = read_control_par(cpar_fname);
-    tr->seq_par = read_sequence_par(seq_par_fname, tr->cpar->num_cams);
+    if (tr->cpar->num_cams == 1){
+        tr->seq_par = read_sequence_par(seq_par_fname, 4);
+    } else {
+        tr->seq_par = read_sequence_par(seq_par_fname, tr->cpar->num_cams);
+    }
+    
 }
 
 /* tr_free deallocates all data allocated inside a TrackingRun object (but NOT
